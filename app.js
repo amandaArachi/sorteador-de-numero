d01adfb70daca99d63049871a93a7d771edfd785
amandaArachi <amanda.arachi@hotmail.com>
2025-06-13 12:44:12 -0300

    exibir número sorteado

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ function sortear() {
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
-    
+
     let sorteados = [];
     let numero;
 
     for (let i = 0; i < quantidade; i++) {
         numero = gerarNumeroAleatorio(de, ate);
+
+        while (sorteados.includes(numero)) {
+            numero = gerarNumeroAleatorio(de, ate);
+        }
+
         sorteados.push(numero);
 }
 
-alert(sorteados);
-
+let resultado = document.getElementById('resultado');
+resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
 }
 
 function gerarNumeroAleatorio(min, max) {
